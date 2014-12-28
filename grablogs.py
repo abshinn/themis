201a5b2628010e39e4bbaac5ae950834aa10acd9
Adam Shinn <adam.b.shinn@gmail.com>
2014-12-28 10:15:23 -0800

    Move output to curdir/processed_logs; add tha/thb to log filename.

--- grablogs.py
+++ grablogs.py
@@ -3,16 +3,17 @@
 import os
 
 def main():
+    todir = os.getcwd()
     if os.path.isdir("themis.ssl.berkeley.edu/"):
         for root, dirs, files in os.walk("themis.ssl.berkeley.edu/"):
             if len(files) > 0:
                 if files[0] == "file_splitter_cmpstats.log":
                     path = "/".join([root, files[0]])
                     if os.path.getsize( path ) > 0:
-                        name = root.split("/")[-1].split(".")[-1]
-                        os.system( "mkdir -p ~/themis/logs" )
-                        os.system( "cp %s ~/themis/logs/%s_cmpstats.log" % (path, name) )
-                        print "created: ~/themis/logs/%s_cmpstats.log" % (name,)
+                        name = root.split("/")[-5] + "_" +  root.split("/")[-1].split(".")[-1]
+                        os.system( "mkdir -p %s/processed_logs" % (todir,) )
+                        os.system( "cp %s %s/processed_logs/%s_cmpstats.log" % (path, todir, name) )
+                        print "created: %s/processed_logs/%s_cmpstats.log" % (todir, name)
     else:
         raise Exception("themis.ssl.berkeley.edu/ directory not found")
 
